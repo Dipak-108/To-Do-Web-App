7caaa18880ccb211abeb3f0c3a0da6da788f346a
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-07-16 19:07:03 +0545

    todo app complete

--- script.js
+++ script.js
@@ -3,28 +3,22 @@ let form = document.getElementsByClassName("todo_form")[0];
 let input = document.getElementById("todo_input");
 let error_msg = document.getElementById("error_msg");
 let clearAll = document.getElementById("btn_clear");
-let toDoList=[];
+let allTodoList = document.getElementsByClassName("todoImg");
+let toDoList = [];
 
-// since toDoList was emptied upon window reload, this ensures todoList retrives values from
-// local storage if window is reloaded
-function retrieveTodoReload(){
-  if(localStorage.getItem("todos")==null){
-     toDoList=[];
-  }
-  
-  else{
-    toDoList=[...JSON.parse(localStorage.getItem("todos"))]
-    
+/* since toDoList was again initialized as empty array upon window reload, this ensures todoList retrives values from
+ local storage if window is reloaded*/
+function retrieveTodoReload() {
+  if (localStorage.getItem("todos") == null) {
+    toDoList = [];
+  } else {
+    toDoList = [...JSON.parse(localStorage.getItem("todos"))];
   }
   return toDoList;
-};
-retrieveTodoReload()
+}
+retrieveTodoReload();
 
-
-
-
-// function to clear todolist, eachtime the list is shown using foreach loop, this clears the
-// list so that no duplication is found
+/*  function to clear todolist, eachtime the list is shown using foreach loop, this clears the list so that no duplication is found */
 function clearTodos() {
   document.getElementById("todo_list").innerHTML = "";
 }
@@ -39,10 +33,14 @@ function checkEmptyValue(check_value) {
   return check_value;
 }
 
-function printTodo() {
+function addTodotoStorage(toDoList){
+  localStorage.setItem("todos", JSON.stringify(toDoList));
+}
+
+// prints todo that are in local storage
+function printTodo(toDoListPrintTODo) {
   if (localStorage.getItem("todos")) {
-    parsedTodo = JSON.parse(localStorage.getItem("todos"));
-    
+    parsedTodo = toDoListPrintTODo;
 
     parsedTodo.forEach((element) => {
       document.getElementById("todo_list").insertAdjacentHTML(
@@ -50,7 +48,7 @@ function printTodo() {
         `<li>
     <div class="todoDiv">
     
-    <div  class="todoDivLeft"> <img src="./images/check_green.png" alt="" width="40px"> </div>
+    <div  class="todoDivLeft"> <img class="todoImg" src="./images/check_green.png" alt="" width="40px" onclick="removeThistodo(event)"> </div>
     <div class="todoDivRight"><p>${element.toDoContent}</p></div>
     </div>
     </li>`
@@ -60,45 +58,52 @@ function printTodo() {
   }
 }
 
-function addTodotoStorage(toDo) {
-  
+// adds todo to localstorage
+function addTodotoArray(toDo) {
   let todoObj = {
     toDoContent: " ",
-    isComplete: false,
   };
+  let checked_todo = checkEmptyValue(toDo);
 
-  todoObj.toDoContent = toDo;
-
-  toDoList=retrieveTodoReload();
+  todoObj.toDoContent = checked_todo;
+  toDoList = retrieveTodoReload();
   toDoList.unshift(todoObj);
 
-  if (toDo !== false) {
-    localStorage.setItem("todos", JSON.stringify(toDoList));
+  if (checked_todo !== false) {
+    addTodotoStorage(toDoList)
   }
-  return toDoList;
-
 }
 
 // event listener for adding todo in local Storage
 addTodo.addEventListener("click", function (event) {
   event.preventDefault();
   todoInput = checkEmptyValue(input.value);
-  addTodotoStorage(todoInput);
+  addTodotoArray(todoInput);
   form.reset();
   clearTodos();
-  printTodo();
+  printTodo(toDoList);
 });
 clearTodos();
-printTodo();
+printTodo(toDoList);
 
+// clears all todo list
 clearAll.addEventListener("click", function (event) {
   localStorage.clear();
   clearTodos();
   toDoList = [];
 });
 
+function removeThistodo(event) {
+  let abc =
+    event.target.parentElement.parentElement.childNodes[3].childNodes[0].innerHTML;
 
-// document.getElementsByClassName("todoDivLeft").addEventListener("click",function (event){
-//   console.log(this.event);
-// })
-
+  toDoList.forEach((element) => {
+    index = toDoList.indexOf(element);
+    if (element.toDoContent === abc) {
+      toDoList.splice(index, 1);
+    }
+    clearTodos();
+    addTodotoStorage(toDoList);
+    printTodo(toDoList);
+  });
+}
