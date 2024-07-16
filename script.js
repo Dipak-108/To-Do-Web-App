99efb0dd1da64840d7d6cae7e250e3b0af2b1b48
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-07-16 15:03:26 +0545

    todo add and display functionality complete

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let form = document.getElementsByClassName("todo_form")[0];
 let input = document.getElementById("todo_input");
 let error_msg = document.getElementById("error_msg");
 let clearAll = document.getElementById("btn_clear");
-let toDoList = [];
+
+let toDoList=[];
+
+
+
+// function to clear todolist, eachtime the list is shown using foreach loop, this clears the
+// list so that no duplication is found
+function clearTodos() {
+  document.getElementById("todo_list").innerHTML = "";
+}
 
 function checkEmptyValue(check_value) {
   if (!check_value) {
@@ -16,33 +25,56 @@ function checkEmptyValue(check_value) {
 }
 
 function printTodo() {
-  if (localStorage.getItem("todo")) {
-    localStorageTodo = localStorage.getItem("1");
-    document
-      .getElementById("todo_list")
-      .insertAdjacentHTML("beforeend", `<li>${localStorage.getItem("1")}</li>`);
+  if (localStorage.getItem("todos")) {
+    parsedTodo = JSON.parse(localStorage.getItem("todos"));
+
+    parsedTodo.forEach((element) => {
+      document.getElementById("todo_list").insertAdjacentHTML(
+        "beforeend",
+        `<li>
+    <div class="todoDiv">
+    
+    <div class="todoDivLeft"> <img src="./images/check_green.png" alt="" width="40px"> </div>
+    <div class="todoDivRight"><p>${element.toDoContent}</p></div>
+    </div>
+    </li>`
+      );
+      return parsedTodo;
+    });
   }
 }
 
-function addTodotoStorage(toDo) {
-  toDoList.unshift(toDo);
+function addTodotoStorage(toDo,toDoList) {
+  
+  let todoObj = {
+    toDoContent: " ",
+    isComplete: false,
+  };
+
+  todoObj.toDoContent = toDo;
+  toDoList.unshift(todoObj);
+
   if (toDo !== false) {
-    localStorage.setItem("todo", toDoList);
+    localStorage.setItem("todos", JSON.stringify(toDoList));
   }
+  return toDoList;
+
 }
 
 // event listener for adding todo in local Storage
 addTodo.addEventListener("click", function (event) {
   event.preventDefault();
   todoInput = checkEmptyValue(input.value);
-  addTodotoStorage(todoInput);
+  addTodotoStorage(todoInput,toDoList);
   form.reset();
+  clearTodos();
   printTodo();
 });
+clearTodos();
 printTodo();
 
 clearAll.addEventListener("click", function (event) {
   localStorage.clear();
-  document.getElementById("todo_list").innerHTML = "";
+  clearTodos();
   toDoList = [];
 });
